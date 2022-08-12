b7243fabed457ee59009a216c42fa9313258f2c7
niikoolaa04 <nikola04jovicic@gmail.com>
2022-08-12 14:29:36 +0200

    Minecraft Data

---
+++ src/structures/minecraft/MinecraftData.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export default class MinecraftData {
+  /**
+   * Base URL for Fetching Minecraft Data
+   * URL Structure: /pc/<version>/<type - items, entities..>.json
+   */
+   private BASE_URL = "https://raw.githubusercontent.com/PrismarineJS/minecraft-data/master/data/pc/"
+
+  /**
+   * Fetch Minecraft Data
+   * 
+   * @param {string} endpoint - Endpoint
+   * @return {any} Minecraft Data
+   */
+  async _get(endpoint: string, url: string = this.BASE_URL) {
+    if(!endpoint || endpoint == "") throw new Error("Invalid Endpoint/Search Query have been provided.");
+
+    return axios.get(url == this.BASE_URL ? url + endpoint : url, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((res) => res.data).catch((err) => {
+      throw new Error("Invalid Endpoint/Search Query have been provided.");
+    });
+  }
+}
---
+++ src/types/Minecraft.ts
@@ -0,0 +1,54 @@
+export type MinecraftVersion = "1.8  "| "1.9" | "1.10" | "1.11" 
+  | "1.12" | "1.13" | "1.14" | "1.15" | "1.16" | "1.17"
+  | "1.18" | "1.19"
+
+export interface MinecraftItems {
+  id: number,
+  displayName: string,
+  name: string,
+  stackSize: number
+}
+
+export interface MinecraftEntities {
+  id: number,
+  internalId: number,
+  name: string,
+  dispalyName: string,
+  width: number,
+  height: number,
+  type: string,
+  category: string
+}
+
+export interface MinecraftBlocks {
+  id: number,
+  displayName: string,
+  name: string,
+  hardness: number,
+  resistance: number,
+  minStateId: number,
+  maxStateId: number,
+  states: Array<any>,
+  drops: Array<any>,
+  diggable: boolean,
+  transparent: boolean,
+  filterLight: number,
+  emitLight: number,
+  boundingBox: string,
+  stackSize: number,
+  material: string,
+  defaultState: number,
+}
+
+export interface MinecraftBiomes {
+  id: number,
+  name: string,
+  category: string,
+  temperature: number,
+  precipitation: string,
+  depth: number | string,
+  dimension: string,
+  dispalyName: string,
+  color: number,
+  rainfall: number
+}
